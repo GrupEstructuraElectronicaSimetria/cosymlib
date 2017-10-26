2bb48dda096f1ec6704ebfe14bc4048a07d51970
efrem <komuisan@gmail.com>
2017-10-26 13:00:52 +0900

    visual changes shape references

--- symeess/shape/__init__.py
+++ symeess/shape/__init__.py
@@ -8,7 +8,7 @@ def get_measure(geometry):
     """
     Compute the shape measure of the given geometry
 
-    :param geometry: this object conta  ins information about the positions of the molecule as well as the information
+    :param geometry: this object contains information about the positions of the molecule as well as the information
                      needed to carry out the shape's module
     :return: difference between user's structure and the one's that is compared. While 0 is no difference and 100
              is completly different
