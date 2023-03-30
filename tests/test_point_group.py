406d81cb1726e1f4f81dcec753a5762c994a091b
Abel Carreras <abelcarreras83@gmail.com>
2023-03-30 23:55:27 +0200

    fix test for point group

--- tests/test_point_group.py
+++ tests/test_point_group.py
@@ -9,7 +9,7 @@ data_dir = os.path.join(os.path.dirname(__file__), 'data')
 class TestPointGroup(unittest.TestCase):
 
     def test(self):
-        point_groups = ['C1', 'Cs', 'Ci', 'Cinfh', 'Dinfh', 'C2', 'C3', 'C2h', 'C3h', 'C2v', 'C3v', 'C4v', 'C5v', 'D2',
+        point_groups = ['C1', 'Cs', 'Ci', 'Cinfv', 'Dinfh', 'C2', 'C3', 'C2h', 'C3h', 'C2v', 'C3v', 'C4v', 'C5v', 'D2',
                         'D3', 'D2h', 'D3h', 'D4h', 'D5h', 'D6h', 'D7h', 'D8h', 'D2d', 'D3d', 'D4d', 'D5d', 'D6d', 'D8d',
                         'S4', 'T', 'Th', 'Td', 'O', 'Oh']
         molecules = file_io.read_generic_structure_file(data_dir + '/point_group/sym_molecules.xyz', read_multiple=True)
